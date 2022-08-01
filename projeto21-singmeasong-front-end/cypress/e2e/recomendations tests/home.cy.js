3540f655904be51fc7d1278e87603c7caf924cf0
barbarina <barbaramarinaufsm@gmail.com>
2022-08-01 05:14:15 -0300

    test: add e2e test for create recommendations

--- projeto21-singmeasong-front-end/cypress/e2e/recomendations tests/home.cy.js
+++ projeto21-singmeasong-front-end/cypress/e2e/recomendations tests/home.cy.js
@@ -1,15 +1,33 @@
 /// <reference types="cypress" />
 
+import { faker } from "@faker-js/faker";
+
 const URL = "http://localhost:3000";
+const URL_API = "http://localhost:5000";
 
 beforeEach(() => {
     cy.resetDatabase();
 });
 
 describe("home", () => {
-    it ("should navigate to / successfully", () => {
+    it ("should navigate to /", () => {
         cy.visit("/");
         cy.url().should("equal", `${URL}/`);
         cy.get("input").should("be.visible");
+    });
+
+    it("should create recommendation", () => {
+
+        cy.visit("/");
+
+        for (let i = 0; i < 3; i++) {
+            const recommendation = {
+                name: faker.lorem.words(3),
+                youtubeLink: "https://www.youtube.com/watch?v=tMgi_Wlzgew"
+            };
+
+            cy.createRecommendation(recommendation);
+            cy.contains(recommendation.name);
+        }
     });
 });
--- projeto21-singmeasong-front-end/cypress/support/commands.js
+++ projeto21-singmeasong-front-end/cypress/support/commands.js
@@ -1,8 +1,18 @@
 const URL_API = "http://localhost:5000"
+const URL = "http://localhost:3000"
 
 Cypress.Commands.add("resetDatabase", () => {
-    cy.log("reseting recommendations");
     cy.request("DELETE", `${URL_API}/reset`).then(res => {
             cy.log(res);
     });
+});
+
+
+Cypress.Commands.add("createRecommendation", recommendation => {
+    cy.get("input[placeholder='Name']").type(recommendation.name);
+    cy.get("input[placeholder='https://youtu.be/...']").type(recommendation.youtubeLink);
+
+    cy.intercept("POST", `${URL_API}/recommendations`).as("createRecommendation");
+    cy.get("button").click();
+    cy.wait("@createRecommendation");
 });
